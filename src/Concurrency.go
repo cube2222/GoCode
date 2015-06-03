9df6c7c4a0d68f83b86e94c858cdd902c5c61697
Jakub Martin <jakub.wit.martin@gmail.com>
2015-06-03 20:24:11 +0200

    Copyright Test

--- src/Concurrency.go
+++ src/Concurrency.go
@@ -6,14 +6,12 @@ import (
 
 func main() {
 	c := make(chan int, 5)
-
 	for i := 0; i < 5; i++ {
 		go func() {
 			fmt.Println("Hello World")
 			c <- 1
 		}()
 	}
-
 	for i := 0; i < 5; i++ {
 		<-c
 	}
